12a06e05a84df3e80259420b35ba2c63ae10f29c
samezzzz228 <rasulov_aziret>
2022-03-04 23:09:06 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,57 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Main ob = new Main();
+        int a[] = { 5, 1, 4, 2, 8, 0, 2 };
+        ob.cocktailSort(a);
+        System.out.println("Sort");
+        ob.printArray(a);
+    }
+
+             void cocktailSort(int a[])
+            {
+                boolean swapped = true;
+                int start = 0;
+                int end = a.length;
+
+                while (swapped == true)
+                {
+                    swapped = false;
+                    for (int i = start; i < end - 1; ++i)
+                    {
+                        if (a[i] > a[i + 1]) {
+                            int temp = a[i];
+                            a[i] = a[i + 1];
+                            a[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                    if (swapped == false)
+                        break;
+                    swapped = false;
+                    end = end - 1;
+                    for (int i = end - 1; i >= start; i--)
+                    {
+                        if (a[i] > a[i + 1])
+                        {
+                            int temp = a[i];
+                            a[i] = a[i + 1];
+                            a[i + 1] = temp;
+                            swapped = true;
+                        }
+                    }
+                    start = start + 1;
+                }
+            }
+            void printArray(int a[])
+            {
+                int n = a.length;
+                for (int i = 0; i < n; i++)
+                    System.out.print(a[i] + " ");
+                System.out.println();
+            }
+
+        }
+
